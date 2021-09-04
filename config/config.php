c18851b6969b5ca68ee513fe0ce8c37f234286f1
yazshrilld <yazshrilld@gmail.com>
2021-09-04 07:48:25 +0100

    config commit

--- config/config.php
+++ config/config.php
@@ -1,7 +1,15 @@
 <?php 
-define ("DB_HOST", "localhost");
-define ("DB_USER", "root");
-define ("DB_PASS", "");
-define ("DB_NAME", "db_store");
+
+    // my localserver connection
+    // define ("DB_HOST", "localhost");
+    // define ("DB_USER", "root");
+    // define ("DB_PASS", "");
+    // define ("DB_NAME", "db_store");
+
+    //my heroku development connection for remote SQL
+    define ("DB_HOST", "remotemysql.com");
+    define ("DB_USER", "XI1D76iPcS");
+    define ("DB_PASS", "wDK42OYBUM");
+    define ("DB_NAME", "XI1D76iPcS");
 
  ?> 
--- inc/header.php
+++ inc/header.php
@@ -25,7 +25,7 @@
 
 <!DOCTYPE HTML>
 <head>
-<title>Store Website</title>
+<title>Yazid's Commerce</title>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 <link href="css/style.css" rel="stylesheet" type="text/css" media="all"/>
